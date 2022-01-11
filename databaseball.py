db80d2c00c895493fc4faaf12009a666d9fd6ccf
Barry Zhang <45883553+bluebarryz@users.noreply.github.com>
2022-01-11 18:59:26 -0500

    fixed dropdown bugs

--- databaseball.py
+++ databaseball.py
@@ -1,11 +1,10 @@
 from h2o_wave import main, app, Q, ui, data
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import math
 
 players = [["Ted Williams", "tedWil"], ["Mike Trout", 'trout']]
-stategories = ['Year', 'Age', 'G', 'BA', 'HR', 'RBI', 'OBP', 'SLG', 'OPS', 'OPS_Plus', 'Awards']
+stategories = ['Year', 'Age', 'Games', 'BA', 'HR', 'RBI', 'OBP', 'SLG', 'OPS', 'OPS_Plus', 'Awards']
+stategories_proper_text = [stat if stat != 'OPS_Plus' else 'OPS+' for stat in stategories]
+
 
 """['Jackie Robinson', 'robinson'], 
     ['Albert Pujols', 'pujols'], ['Babe Ruth', 'ruth'], ['Hank Aaron', 'aaron'], 
@@ -23,14 +22,15 @@ def QQ_metric(ops_plus, games):
 
 for player in players:
     loaded_data = load_data(player[1])
+    loaded_data = loaded_data.rename(columns={"G": "Games"})
     player_sheets.setdefault(player[0], loaded_data.loc[:len(loaded_data)-3, stategories])
 
     qq_data = {
         "Year": player_sheets[player[0]]["Year"],
         "Age": player_sheets[player[0]]["Age"],
         "QQ Metric": [QQ_metric(a,b) for a,b in zip(player_sheets[player[0]]['OPS_Plus'], 
-                                                    player_sheets[player[0]]['G'])],
-        "Games": player_sheets[player[0]]["G"],
+                                                    player_sheets[player[0]]['Games'])],
+        "Games": player_sheets[player[0]]["Games"],
         "OPS+": player_sheets[player[0]]["OPS_Plus"],
         
     }
@@ -41,8 +41,9 @@ for player in players:
 
 @app('/databaseball')
 async def serve(q:Q):
-    print(q.args.stat_category, q.args.player_dropdown)
+    # print(q.args)
     if not q.client.initialized:
+        print("init")
         initialize_ui(q)
         q.client.stat_category = 'HR'
         q.client.time = 'Age'
@@ -51,23 +52,34 @@ async def serve(q:Q):
         q.client.df_qq = qq_sheets[q.client.player]
         graph_view(q, q.client.df)
         qq_table(q, q.client.df_qq)
+    
+    print(q.args.player_dropdown, q.client.player)
+    if q.args.player_dropdown == q.client.player:
+        print("same")
+        q.args.player_dropdown = None
 
+    # print(q.args.player_dropdown)
     if q.args.graph:
-        graph_view(q, q.client.df)
+        # print(q.args.graph, "graph")
+        graph_view(q, q.client.df)   
     elif q.args.table:
+        # print(q.args.table, "table")
         table_view(q, q.client.df)    
     elif q.args.player_dropdown:
+        # print(q.args.player_dropdown, "player dropdown")
         q.client.player = q.args.player_dropdown
         q.client.df = player_sheets[q.client.player]
         q.client.df_qq = qq_sheets[q.client.player]
         graph_view(q, q.client.df)  
         qq_table(q, q.client.df_qq)
     elif q.args.stat_category or q.args.time:
+        # print("el")
         q.client.stat_category = q.args.stat_category
         q.client.time = q.args.time
         graph_view(q, q.client.df)
     
-
+    q.args.player_dropdown = None 
+    # print(q.args.stat_category, q.args.player_dropdown)  
     await q.page.save()
         
 
@@ -117,25 +129,24 @@ def initialize_ui(q):
         ]
     )
     q.client.initialized = True
-    
-
 
 
 
 
 
 def qq_table(q, df_qq):
+    # print("creating qq_table")
     q.page['players'] = ui.form_card(
         box="players",
         items=[
             ui.dropdown(
                 name='player_dropdown',
-                label='Select Player',
-                value=q.client.player,
+                label='Select Player',               
                 choices=[
                     ui.choice(name=player[0], label=player[0]) for player in players
                 ],
-                trigger=True
+                trigger=True,
+                value=q.client.player,
             )
         ]
     )
@@ -158,13 +169,12 @@ def qq_table(q, df_qq):
 
 def table_view(q, df):
     del q.page["graph_view"], q.page["statsDropdown"]
-
     q.page['table_view'] = ui.form_card(
         box='data', 
         items=[
             ui.table(
                 name='table example',
-                columns=[ui.table_column(name=col, label=col) for col in df.columns],
+                columns=[ui.table_column(name=col, label=col) for col in stategories_proper_text],
                 rows=[ui.table_row(
                     name=str(i),
                     cells=[str(df[col].values[i]) for col in df.columns]
@@ -185,28 +195,28 @@ def graph_view(q, df):
             ui.inline(items=[
                 ui.dropdown(
                     name='stat_category',
-                    label='y-axis (Stat Category)',
-                    value=q.client.stat_category,
+                    label='y-axis (Stat Category)',                   
                     choices=[
                         ui.choice(name=col, label=col) for col in df.columns.values[2:-1]
                     ],
                     width='300px',
-                    trigger=True
+                    trigger=True,
+                    value=q.client.stat_category,
                 ),
 
                 ui.dropdown(
                     name='time',
-                    label='x-axis (Time)',
-                    value=q.client.time,
+                    label='x-axis (Time)',                   
                     choices=[
                         ui.choice(name=col, label=col) for col in ['Year','Age']
                     ],
-                    trigger=True
+                    trigger=True,
+                    value=q.client.time,
                 )
             ])
         ]
     )
-    
+
     q.page['graph_view']= ui.plot_card(
         box = 'data',
         title=f'{q.client.player}\'s {q.client.stat_category}',
@@ -214,7 +224,7 @@ def graph_view(q, df):
         plot = ui.plot(marks=[ui.mark(
             type='interval',
             x=f'={q.client.time}',
-            y=f'={q.client.stat_category}',
+            y=f'=OPS',
             x_title=q.client.time,
             y_title=q.client.stat_category
         ),
